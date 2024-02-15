9a5521fec28b18422318adcb6ccce503433881e7
D1mkadon <dima0938157970@gmail.com>
2024-02-15 19:37:24 +0100

    12

---
+++ src/pages/Home.jsx
@@ -0,0 +1,54 @@
+import { motion, AnimatePresence } from "framer-motion";
+import { useSnapshot } from "valtio";
+import state from "../store";
+import {
+  headContainerAnimation,
+  headTextAnimation,
+  headContentAnimation,
+  slideAnimation,
+} from "../config/motion";
+import CustomButton from "../components";
+
+const Home = () => {
+  const snap = useSnapshot(state);
+  state;
+  return (
+    <AnimatePresence>
+      {snap.intro && (
+        <motion.section className="home" {...slideAnimation("left")}>
+          <motion.header {...slideAnimation("down")}>
+            <img
+              src="./threejs.png"
+              alt="logo"
+              className="w-8 h-8 object-contain"
+            />
+          </motion.header>
+          <motion.div className="home-content" {...headContainerAnimation}>
+            <motion.div {...headTextAnimation}>
+              <h1 className="head-text">
+                let`s <br className="xl:block hidden" /> do it
+              </h1>
+            </motion.div>
+            <motion.div
+              className="flex flex-col gap-5"
+              {...headContentAnimation}
+            >
+              <p className="max-w-md font-normal text-gray-600 text-base">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae
+                voluptates perferendis <strong>eaque quisquam enim</strong> quia
+                corporis doloribus repellat sint sequi.
+              </p>
+              <CustomButton
+                type="filled"
+                title="Customize it"
+                handleClick={() => (state.intro = false)}
+              />
+            </motion.div>
+          </motion.div>
+        </motion.section>
+      )}
+    </AnimatePresence>
+  );
+};
+
+export default Home;
